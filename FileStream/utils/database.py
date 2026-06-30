26ec7b0e883fdf0a93e827614d271334910ccc4b
Thanos5544 <thanos554433@gmail.com>
2026-06-30 17:46:08 +0530

    Update database.py

--- FileStream/utils/database.py
+++ FileStream/utils/database.py
@@ -66,13 +66,13 @@ class Database:
         count = await self.black.count_documents({})
         return count
         # ---------------------[ ADD FILE TO DB ]---------------------#
-async def add_file(self, file_info):
-    file_info["time"] = time.time()
+    async def add_file(self, file_info):
+        file_info["time"] = time.time()
 
-    await self.count_links(file_info["user_id"], "+")
+        await self.count_links(file_info["user_id"], "+")
 
-    return (await self.file.insert_one(file_info)).inserted_id
-    
+        return (await self.file.insert_one(file_info)).inserted_id
+        
 # ---------------------[ FIND FILE IN DB ]---------------------#
     async def find_files(self, user_id, range):
         user_files=self.file.find({"user_id": user_id})
