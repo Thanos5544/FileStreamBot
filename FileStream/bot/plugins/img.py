cacc078d133b5efa52c95250ff47c3b657aa2b7e
Thanos5544 <thanos554433@gmail.com>
2026-07-01 12:04:47 +0530

    Update img.py

--- FileStream/bot/plugins/img.py
+++ FileStream/bot/plugins/img.py
@@ -1,16 +1,12 @@
 import aiohttp
 
 from pyrogram import filters
-from pyrogram.types import (
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-    InputMediaPhoto
-)
+from pyrogram.types import InputMediaPhoto
 
 from FileStream.bot import FileStream
 
 
-TMDB_API = "18303910643c603ebb9e370f2f49db56"
+ACCESS_KEY = "99JuNaSj-p9GZio8IAUtXTpsk_ySalCFrJ1GxU0EvtM"
 
 
 @FileStream.on_message(filters.command("img"))
@@ -18,159 +14,75 @@ async def img_search(_, m):
 
     if len(m.command) < 2:
         return await m.reply_text(
-            "❌ ᴜsᴇ:\n\n/img movie name"
+            "❌ USE:\n\n/img movie name"
         )
 
-    name = " ".join(m.command[1:])
+    query = " ".join(m.command[1:])
+
 
     msg = await m.reply_text(
-        "🔎 sᴇᴀʀᴄʜɪɴɢ..."
+        "⬆️ Uploading 20 images to Telegram..."
     )
+
 
     try:
 
+        url = (
+            "https://api.unsplash.com/search/photos"
+            f"?query={query}"
+            "&per_page=20"
+            f"&client_id={ACCESS_KEY}"
+        )
+
+
         async with aiohttp.ClientSession() as session:
 
-            url = (
-                "https://api.themoviedb.org/3/search/multi"
-                f"?api_key={TMDB_API}&query={name}"
-            )
+            async with session.get(url) as resp:
 
-            async with session.get(url) as r:
-                data = await r.json()
+                data = await resp.json()
 
 
-        results = data.get("results", [])[:8]
+        results = data.get("results", [])
 
 
         if not results:
+
             return await msg.edit_text(
-                "❌ ɴᴏ ʀᴇsᴜʟᴛ"
+                "❌ No Images Found"
             )
 
 
-        buttons = []
+        media = []
 
 
-        for x in results:
+        for item in results[:20]:
 
-            title = x.get("title") or x.get("name")
+            photo = item["urls"]["regular"]
 
-            typ = x.get("media_type")
-
-            if typ not in ["movie","tv"]:
-                continue
-
-
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        f"🎬 {title}",
-                        callback_data=f"img#{typ}#{x['id']}"
-                    )
-                ]
+            media.append(
+                InputMediaPhoto(photo)
             )
 
 
-        await msg.edit_text(
-            "🎬 sᴇʟᴇᴄᴛ ᴛɪᴛʟᴇ 👇",
-            reply_markup=InlineKeyboardMarkup(buttons)
+        await m.reply_media_group(media)
+
+
+        await m.reply_text(
+f"""
+🖼 **IMAGES FOR:** `{query}`
+
+• SOURCE: Unsplash
+"""
         )
+
+
+        await msg.delete()
 
 
     except Exception as e:
+
         print(e)
+
         await msg.edit_text(
-            "❌ sᴇᴀʀᴄʜ ᴇʀʀᴏʀ"
-        )
-
-
-
-
-@FileStream.on_callback_query(filters.regex("^img#"))
-async def img_callback(_, q):
-
-    await q.answer("📸 ɢᴇɴᴇʀᴀᴛɪɴɢ...")
-
-
-    try:
-
-        _, typ, mid = q.data.split("#")
-
-
-        async with aiohttp.ClientSession() as session:
-
-            url = (
-                f"https://api.themoviedb.org/3/{typ}/{mid}"
-                f"?api_key={TMDB_API}"
-            )
-
-            async with session.get(url) as r:
-                info = await r.json()
-
-
-
-            imgurl = (
-                f"https://api.themoviedb.org/3/{typ}/{mid}/images"
-                f"?api_key={TMDB_API}"
-            )
-
-            async with session.get(imgurl) as r:
-                images = await r.json()
-
-
-
-        pics = []
-
-
-        if info.get("poster_path"):
-            pics.append(
-                "https://image.tmdb.org/t/p/w780"
-                + info["poster_path"]
-            )
-
-
-        if info.get("backdrop_path"):
-            pics.append(
-                "https://image.tmdb.org/t/p/w780"
-                + info["backdrop_path"]
-            )
-
-
-        for x in images.get("posters", []):
-            pics.append(
-                "https://image.tmdb.org/t/p/w780"
-                + x["file_path"]
-            )
-
-
-        for x in images.get("backdrops", []):
-            pics.append(
-                "https://image.tmdb.org/t/p/w780"
-                + x["file_path"]
-            )
-
-
-        pics = list(dict.fromkeys(pics))[:20]
-
-
-        if not pics:
-            return await q.message.reply_text(
-                "❌ ɪᴍᴀɢᴇ ɴᴏᴛ ғᴏᴜɴᴅ"
-            )
-
-
-        await q.message.reply_media_group(
-            [
-                InputMediaPhoto(media=p)
-                for p in pics
-            ]
-        )
-
-
-    except Exception as e:
-        print(e)
-
-        await q.message.reply_text(
-            "❌ ɪᴍᴀɢᴇ ᴇʀʀᴏʀ"
+            "❌ RESULT ERROR"
         )
