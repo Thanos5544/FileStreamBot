2d04f0f8a77549f2d7daa9f6484b09dcd501f27c
Thanos5544 <thanos554433@gmail.com>
2026-07-07 18:03:12 +0530

    Update movie_post.py

--- FileStream/bot/plugins/movie_post.py
+++ FileStream/bot/plugins/movie_post.py
@@ -110,7 +110,7 @@ def create_left_color_gradient(size, color_rgb):
         if x < w * 0.45:
             alpha = 150
         elif x < w * 0.65:
-            fade = (x - w * 0.5) / (w * 0.2)
+            fade = (x - w * 0.45) / (w * 0.20)
             alpha = int(200 * (1 - fade))
         else:
             alpha = 0
