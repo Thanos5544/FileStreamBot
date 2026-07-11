2bb2fc585be5199a958c1699cfb7f53bf148fcaf
Thanos5544 <thanos554433@gmail.com>
2026-07-11 12:18:48 +0530

    Update database.py

--- FileStream/utils/database.py
+++ FileStream/utils/database.py
@@ -1,25 +1,19 @@
 import motor.motor_asyncio
-import datetime
-from typing import Dict, List
 
 
 class Database:
-    def __init__(self, uri: str, database_name: str):
+    def __init__(self, uri, db_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
+        self.db = self._client[db_name]
         self.col = self.db["users"]
         self.file = self.db["files"]
 
-    # ---------------- USER ---------------- #
-    async def add_user(self, user_id: int):
+    async def add_user(self, user_id: int, u_type="user"):
         await self.col.update_one(
             {"id": user_id},
-            {"$set": {"id": user_id, "type": "user"}},
+            {"$set": {"id": user_id, "type": u_type}},
             upsert=True
         )
-
-    async def delete_user(self, user_id: int):
-        await self.col.delete_one({"id": user_id})
 
     async def get_user(self, user_id: int):
         return await self.col.find_one({"id": user_id})
@@ -30,60 +24,45 @@ class Database:
     async def total_users_count(self):
         return await self.col.count_documents({"type": "user"})
 
-    async def is_user_banned(self, user_id: int):
-        user = await self.col.find_one({"id": user_id, "banned": True})
-        return bool(user)
-
-    async def ban_user(self, user_id: int):
-        await self.col.update_one(
-            {"id": user_id},
-            {"$set": {"banned": True}},
-            upsert=True
-        )
-
-    async def unban_user(self, user_id: int):
-        await self.col.update_one(
-            {"id": user_id},
-            {"$set": {"banned": False}},
-            upsert=True
-        )
-
-    async def total_banned_users_count(self):
-        return await self.col.count_documents({"banned": True})
-
-    # ---------------- GROUPS / CHANNELS ---------------- #
     async def total_groups_count(self):
         return await self.col.count_documents({"type": "group"})
 
     async def total_channels_count(self):
         return await self.col.count_documents({"type": "channel"})
 
-    async def get_all_by_type(self, user_type: str):
-        return self.col.find({"type": user_type})
+    async def get_all_by_type(self, t):
+        return self.col.find({"type": t})
 
-    # ---------------- FILES ---------------- #
-    async def add_file(self, file_data: Dict):
-        await self.file.insert_one(file_data)
+    async def is_user_banned(self, user_id: int):
+        u = await self.col.find_one({"id": user_id, "banned": True})
+        return bool(u)
 
-    async def get_file(self, file_id: str):
-        return await self.file.find_one({"_id": file_id})
+    async def ban_user(self, user_id: int):
+        await self.col.update_one({"id": user_id}, {"$set": {"banned": True}}, upsert=True)
 
-    async def delete_one_file(self, file_id: str):
-        await self.file.delete_one({"_id": file_id})
+    async def unban_user(self, user_id: int):
+        await self.col.update_one({"id": user_id}, {"$set": {"banned": False}}, upsert=True)
+
+    async def total_banned_users_count(self):
+        return await self.col.count_documents({"banned": True})
+
+    async def delete_user(self, user_id: int):
+        await self.col.delete_one({"id": user_id})
+
+    async def add_file(self, data):
+        await self.file.insert_one(data)
+
+    async def get_file(self, fid):
+        return await self.file.find_one({"_id": fid})
+
+    async def delete_one_file(self, fid):
+        await self.file.delete_one({"_id": fid})
 
     async def total_files(self):
         return await self.file.count_documents({})
 
-    async def count_links(self, user_id: int, action: str):
+    async def count_links(self, uid, action):
         if action == "+":
-            await self.col.update_one(
-                {"id": user_id},
-                {"$inc": {"Links": 1}},
-                upsert=True
-            )
-        elif action == "-":
-            await self.col.update_one(
-                {"id": user_id},
-                {"$inc": {"Links": -1}},
-                upsert=True
-            )
+            await self.col.update_one({"id": uid}, {"$inc": {"Links": 1}}, upsert=True)
+        else:
+            await self.col.update_one({"id": uid}, {"$inc": {"Links": -1}}, upsert=True)
